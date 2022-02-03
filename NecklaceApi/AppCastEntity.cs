6b42153ebe65d437f1be6a63d7393520f008b29c
John Clayton <johnclayton72@gmail.com>
2022-02-03 06:34:47 +0000

    Added table entity, but need a real editor now

---
+++ NecklaceApi/AppCastEntity.cs
@@ -0,0 +1,40 @@
+﻿namespace Necklace.Api;
+
+public class AppCast 
+{
+    public DateTime CreatedTime { get; set; }
+    public string Description { get; set; }
+    public bool ProductName { get; set; }
+}
+
+public class AppCastEntity : TableEntity
+{
+    public DateTime CreatedTime { get; set; }
+    public string Description { get; set; }
+    public bool ProductName { get; set; }
+}
+
+public static class Mappings
+{
+    public static AppCastEntity ToTableEntity(this AppCast cast)
+    {
+        return new AppCastEntity
+        {
+            PartitionKey = "APPCAST",
+            RowKey = cast.Id,
+            Description = cast.Description,
+            ProductName = cast.ProductName,
+        };
+    }
+
+    public static AppCast FromTableEntity(this AppCastEntity cast)
+    {
+        return new AppCast
+        {
+            Id = cast.RowKey,
+            CreatedTime = cast.CreatedTime,
+            Description = cast.Description,
+            ProductName = cast.ProductName
+        };
+    }
+}
